931d944628ee8d3ba76d2d1097ed6f6b59b3c381
Ayush Dhiman <144990779+ayushdhiman320@users.noreply.github.com>
2023-12-05 15:32:11 +0000

    Create p_17(system_info).sh

---
+++ p_17(system_info).sh
@@ -0,0 +1,13 @@
+# Script that display various system information like the current date and time, loggin-in user, system uptime, etc. 
+
+echo "Current date and time:"
+date
+
+echo "Logged-in user:"
+who
+
+echo "System update time:"
+uptime
+
+echo "Memory usage:"
+free -h
