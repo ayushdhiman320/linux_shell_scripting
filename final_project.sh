2548320aef1b1d29c3e6e9c7282434ce9dbab548
Ayush Dhiman <144990779+ayushdhiman320@users.noreply.github.com>
2023-12-05 21:51:26 +0530

    Update final_project.sh

--- final_project.sh
+++ final_project.sh
@@ -3,12 +3,16 @@
 read "Please enter your symptoms separated by spaces: " symptoms
 symptoms=${symptoms,,}
 
-for symptom in $symptoms; do
-    if [[ $symptom == "fever" ]]; then
+for symptom in $symptoms
+do
+    if [[ $symptom == "fever" ]]
+    then
         echo "Recommendation: Take a fever reducer medication."
-    elif [[ $symptom == "sore throat" ]]; then
+    elif [[ $symptom == "sore throat" ]] 
+    then
         echo "Recommendation: Gargle with warm saltwater."
-    elif [[ $symptom == "cough" && $symptom == "congestion" ]]; then
+    elif [[ $symptom == "cough" && $symptom == "congestion" ]]
+    then
         echo "Recommendation: Drink warm fluids and take cough syrup."
     else
         echo "General Recommendation: Consult a doctor for further advice."
